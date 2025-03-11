6ca4df1b77ad89e173ca4f48bd4dc10109a4a0c0
sbrezo <brezovac.sasa@gmail.com>
2025-03-11 10:11:54 +0100

    Initial Commit

---
+++ RSS Feed AppUITests/RSS_Feed_AppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  RSS_Feed_AppUITestsLaunchTests.swift
+//  RSS Feed AppUITests
+//
+//  Created by Saša Brezovac on 11.03.2025..
+//
+
+import XCTest
+
+final class RSS_Feed_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
